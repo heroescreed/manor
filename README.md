790e8a351284868789c3dc8a3ce0a3d3d226894e
amnexya <jack@eilles.xyz>
2026-09-13 20:38:18 +0100

    feat: loads of commands, loads of removals, bot ready to go though

--- README.md
+++ README.md
@@ -11,21 +11,21 @@ users, managing server roles and logging server updates.
 
 ### General commands
 
-- `!auth` - Starts the authentication process.
-- `!flip` - Flips a coin.
-- `!nucat` - Gets a random image of a cat.
-- `!nudog` - Gets a random image of a dog.
-- `!ran` - Returns a random number between two given values.
-- `!rules` - Displays the server rules.
-- `!give_member` - Assigns the member role. Can only be executed by committee members.
-- `!verified` - Returns a list of verified members display names and ids. Can only be executed by committee members.
-- `!pronouns` - DMs a user with options to change their preferred pronouns.
+- `/auth` - Starts the authentication process.
+- `/cataas` - Gets a random image of a cat from cataas.com
+- `/coinflip` - Flips a coin.
+- `/credits` - Displays developer credits.
+- `!help` - Displays a list of commands, must be run with `!`, not slash command compatible.
+- `/httpcat` - Gets a random image of a cat with a HTTP response code.
+- `/httpdog` - Gets a random image of a dog with a HTTP response code.
+- `/ping` - Shows bot latency.
+- `/roll` - Rolls a dice, can take custom sides and rolls.
+- `/urandom` - Returns a 256 byte string of hexadecimal, fresh from /dev/urandom.
+- `/uwu` - Uwuifies text passed into it, or a random message a few messages before.
 
-### Codewars
+### Committee Only Commands
 
-Commands for managing codewars. These can only be executed in the codewars channel.
-
-- `!challenge` - Sets and announces the next challenge for codewars.
-- `!draw` - Draws the winner of the challenge.
-- `!join` - Links a given codewars account to the server member.
-- `!list_stat` - Returns how many people have completed the current challenge. 
+- `/unverify` - Removes a users verified role.
+- `/unverify_all` - Removes verified from all users in the server.
+- `/verify` - Give the verified role to a user.
+- `/stage_up` - Move all users up one stage. (Stage 1 -> Stage 2 -> Stage 3 -> Alumni)
--- main.py
+++ main.py
@@ -1,18 +1,33 @@
 import asyncio
-from dotenv import load_dotenv
 import os
+import random
+import re
 import smtplib
 import string
-import random
 
 import discord
 from discord.ext import commands
 from discord.ui import Modal, TextInput
+from dotenv import load_dotenv
+from uwuipy import Uwuipy
 
 import ids
-import tools
+
+
+async def check_student_number(student_number: "str") -> "bool":
+    """Checks if a given student number is valid
+
+    :param student_number: String to be checked
+    :return: True if valid, false if invalid
+    """
+    if len(student_number) != 9:
+        return False
+    return bool(re.match(r"^\d{9}$", student_number))
 
 class AuthModal(Modal, title="Verify your Student Status"):
+    """
+    This class is a modal used to verify a user's student status.
+    """
     name = TextInput(
         label="Please enter your name..",
         placeholder="As it appears on your student ID card. (e.g. Alan Turing)",
@@ -36,7 +51,7 @@ class AuthModal(Modal, title="Verify your Student Status"):
 
     async def on_submit(self, interaction: discord.Interaction):
         # Check if their ID is valid
-        if not await tools.check_student_number(self.student_id.value):
+        if not await check_student_number(self.student_id.value):
             await interaction.response.send_message("We're very sorry, but your student number you entered is not valid.\nPlease check it and try again. If it still doesn't work, please open a ticket.", ephemeral=True)
             return
 
@@ -47,19 +62,19 @@ class AuthModal(Modal, title="Verify your Student Status"):
         # Send email to user with verification code
         try:
             smtp_server = os.getenv("SMTP_SERVER")
-            smtp_port = int(os.getenv("SMTP_PORT"))
+            smtp_port = int(os.getenv("SMTP_PORT")) # type: ignore
             smtp_username = os.getenv("SMTP_USERNAME")
             smtp_password = os.getenv("SMTP_PASSWORD")
 
-            with smtplib.SMTP(smtp_server, smtp_port) as server:
+            with smtplib.SMTP(smtp_server, smtp_port) as server: # type: ignore
                 server.starttls()
-                server.login(smtp_username, smtp_password)
+                server.login(smtp_username, smtp_password) # type: ignore
                 server.sendmail(
-                    smtp_username,
+                    smtp_username, # type: ignore
                     f"{self.email.value}@ncl.ac.uk",
                     f"Subject: NUCATS Discord Verification Code\n\nHello {self.name.value},\n\nThank you for starting your verification steps on the NUCATS Discord server.\n\nTo complete your verification, please type the following into the verification channel:\n\nNUCATS{self.student_id.value}{verification_code}\n\nPlease do not share this code with anyone else.\n\nIf you did not request this code, please ignore this email.\n\nThis inbox does accept emails, however replies to this email will be ignored and discarded.\nIf you have any questions, please create a ticket in the server.\n\nKind Regards,\n\nNUCATS Committee."
                 )
-        except Exception as e:
+        except Exception as e: # noqa: BLE001
             print(f"Error sending email: {e}")
             await interaction.response.send_message("We're very sorry, but we couldn't send you a verification email. Please create a ticket to be verified manually.", ephemeral=True)
             return
@@ -74,7 +89,8 @@ class AuthModal(Modal, title="Verify your Student Status"):
             msg = await client.wait_for("message", check=check, timeout=300.0)
             if msg.content == f"NUCATS{self.student_id.value}{verification_code}":
                 await msg.delete()
-                await interaction.user.add_roles(discord.Object(id=ids.verified_role))
+                await asyncio.sleep(1)
+                await interaction.user.add_roles(discord.Object(id=ids.verified_role)) # type: ignore
                 await interaction.followup.send(f"Thank you {interaction.user.mention}! You have been verified. Please check your roles to ensure you have the 'Verified' role.", ephemeral=True)
             else:
                 await interaction.followup.send("The verification code you entered is incorrect. Please try again.", ephemeral=True)
@@ -84,6 +100,8 @@ class AuthModal(Modal, title="Verify your Student Status"):
 # Gets application token
 load_dotenv()
 token = os.getenv("CLIENT_TOKEN")
+
+uwu = Uwuipy()
 
 class NUCATSBot(commands.Bot):
     user: discord.ClientUser
@@ -238,5 +256,148 @@ async def stage_up(ctx):
 
     await ctx.send("All users have been moved up a stage.\nPlease make an announcement to the server to inform members of this change.")
 
-client.run(token)
+@client.hybrid_command(name="coinflip", description="Flips a coin.")
+async def coinflip(ctx):
+    """Flips a coin and returns the result."""
+    # pick heads or tails for user
+    await ctx.send(f"Flipping coin, {ctx.author.mention}, reply with your call (heads or tails).")
+
+    def check(m):
+        return m.author == ctx.author and m.channel == ctx.channel and m.content.lower() in ["heads", "tails"]
+
+    msg = await client.wait_for("message", check=check, timeout=60.0)
+    if msg.content.lower() == "heads":
+        call = "Heads"
+    else:
+        call = "Tails"
+
+    await ctx.send(f"You called {call}. Flipping the coin now...")
+
+    await asyncio.sleep(2)  # wait for dramatic effect
+
+    result = random.choice(["Heads", "Tails"])
+    await ctx.send(f"The coin landed on {result}.")
+
+@client.hybrid_command(name="roll", description="Rolls a die.")
+async def roll(ctx, sides: int = 6, rolls: int = 1):
+    """Rolls a die and returns the result."""
+    if sides < 1 or rolls < 1:
+        await ctx.send("Please enter a valid number of sides and rolls.")
+        return
+
+    results = [random.randint(1, sides) for _ in range(rolls)]
+    await ctx.send(f"You rolled: {', '.join(map(str, results))}")
+    
+
+@client.hybrid_command(name="httpcat", description="Return a random image from http.cat.")
+async def httpcat(ctx):
+    """Returns a random HTTP cat image."""
+    status_codes = [100, 101, 102, 200, 201, 202, 203, 204, 206, 207, 300, 301, 302, 303, 304, 305, 307, 400, 401, 402,
+                    403, 404, 405, 406, 407, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 418, 420,
+                    421, 422, 423, 424, 425, 426, 429,
+                    431,
+                    444,
+                    450,
+                    451,
+                    497,
+                    498,
+                    499,
+                    ]
+    status_code = random.choice(status_codes)
+    await ctx.send(f"https://http.cat/{status_code}")
+
+@client.hybrid_command(name="httpdog", description="Return a random image from http.dog.")
+async def httpdog(ctx):
+    """Returns a random HTTP dog image."""
+    status_codes = [100, 101, 102, 200, 201, 202, 203, 204, 206, 207, 300, 301, 302, 303, 304, 305, 307, 400, 401, 402,
+                        403, 404, 405, 406, 407, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 418, 420,
+                        421, 422, 423, 424, 425, 426, 429,
+                        431,
+                        444,
+                        450,
+                        451,
+                        497,
+                        498,
+                        499,
+                        ]
+    status_code = random.choice(status_codes)
+    await ctx.send(f"https://http.dog/{status_code}.jpg")
+
+@client.hybrid_command(name="credits", description="Shows the credits for the bot.")
+async def credits(ctx):
+    """Shows the credits for the bot."""
+    await ctx.send("```\nBot originally developed by tinyTim567\nRewritten by Jack Eilles (amnexya) with assistance from NUCATS committee.\n```")
+
+@client.hybrid_command(name="urandom", description="Returns a 256-byte string pulled from /dev/urandom")
+async def urandom(ctx):
+    """Returns a 256-byte string pulled from /dev/urandom."""
+    random_bytes = os.urandom(256)  # 256 bytes = 2048 bits
+    random_hex = random_bytes.hex()
+    await ctx.send(f"Here is your 256-byte random string, freshly baked from /dev/urandom: ```\n{random_hex}\n```\nPlease don't actually use this as a secret key, everyone here has seen it.")
+
+@client.hybrid_command(name="8ball", description="A magic 8-ball that can answer anything.")
+async def eight_ball(ctx, *, question: str):
+    """A magic 8-ball that can answer anything."""
+    await ctx.send(f"Question: {question}")
+
+    responses = [
+        "Probably man.",
+        "Idk, ask me later.",
+        "Yes.", 
+        "No.", 
+        "Maybe.",
+        "Think of this as Microsoft Authenticator, that's how bad the outlook is.",
+        "You are asking a computer this, so the answer is probably no.",
+        "Yes, but only if you are a cat.",
+        "No, but only if you are a dog.",
+        "Yes, but only if you are a human.",
+        "No, but only if you are a robot.",
+        "Yes, but only if you are a fish.",
+        "No, but only if you are a bird.",
+        "I'm sorry, but I cannot fulfill your request. As an AI language model, I am programmed to provide helpful and informative responses, but I cannot provide answers that are inappropriate or offensive. Please ask a different question.",
+        "Sure",
+        "Nope",
+        "Absolutely",
+        "Definitely not",
+        "I guess?", 
+        "Outlook hazy, try again later.", 
+        "Yes, in due time.",
+        "boneless chicken",
+        "Not sure about that one.",
+    ]
+
+    # use the question as a seed for the rng but add a bit of noise too
+    question += str(random.randint(1, 100))
+    random.seed(question)
+    response = random.choice(responses)
+    await ctx.send(f"{response}")
+
+@client.hybrid_command(name="bucket", description="This, is a bucket.")
+async def bucket(ctx):
+    """This, is a bucket."""
+    await ctx.send("https://tenor.com/view/team-fortress-2-tf2-tf2-memes-gif-12176472853573905724")
+
+@client.hybrid_command(name="uwu", description="Uwuifies your text.")
+async def uwuify(ctx, text: str | None = None):
+    """Uwuifies the last message sent in the channel, or the message you provide as an argument."""
+
+    if text is None:
+        # Get the last message sent in the channel
+        async for message in ctx.channel.history(limit=2):
+            if message.author != ctx.author:
+                text = message.content
+                break
+
+    uwuified_text = uwu.uwuify(text)
+    await ctx.send(uwuified_text)
+
+
+@client.event
+async def on_member_join(member):
+    """Sends a welcome message to the welcome channel when a user joins the server."""
+    channel = client.get_channel(ids.welcome_channel)
+    await channel.send(f"Welcome {member.mention} to the NUCATS Discord server! Please read the rules and verify yourself using /auth.") # type: ignore
+    await httpcat(channel)  # type: ignore # Send a random http.cat image to the welcome channel
+
+client.run(token) # type: ignore
 

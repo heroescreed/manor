1a5817056832ab9fd99791d81fb30ae86fbbc4dc
heroescreed <100536226+heroescreed@users.noreply.github.com>
2026-09-25 14:16:01 +0100

    Refactor: Moved bot instance to a customisable instance

---
+++ bot/bot.py
@@ -0,0 +1,41 @@
+import discord, os, asyncio
+from discord.ext import commands
+
+from utils.utils import color_message
+from constants import TOKEN
+
+class Bot(commands.AutoShardedBot):
+    def __init__(self):
+        intents = discord.Intents.all()
+        super().__init__(command_prefix="!", intents=intents, help_command=None)
+        self.unloaded_cogs = []
+
+    def initialize(self):
+        os.system("cls")
+        print(color_message(message='''
+ _      _     ____  ____ _____ ____ \n
+/ \  /|/ \ /\/   _\/  _ Y__ __Y ___\ \n
+| |\ ||| | |||  /  | / \| / \ |    \ \n
+| | \||| \_/||  \__| |-|| | | \___ |\n
+\_/  \|\____/\____/\_/ \| \_/ \____/\n 
+            ''', color="yellow"))
+        print(color_message(message="Initializing bot...", color="yellow"))
+        asyncio.run(self.load_extensions())
+        self.run(TOKEN) # type: ignore
+
+    async def load_extensions(self):
+        for filename in os.listdir("./cogs"):
+            if filename.endswith(".py"):
+                try:
+                    await self.load_extension(f"cogs.{filename[:-3]}")
+                    print(color_message(message=f"Loaded extension {filename}", color="green"))
+                except Exception as e:
+                    print(e)
+                    print(color_message(message=f"Failed to load extension {filename}", color="red"))
+                    self.unloaded_cogs.append(filename.capitalize()[-3])
+
+    async def on_ready(self):
+        print(color_message(message=f"Logged in as {self.user}!", color="green"))
+        for shard in self.shards:
+            await self.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.playing, name=f"! | Shard: {shard+1}/{len(self.shards)}"), shard_id=shard)
+                
--- main.py
+++ main.py
@@ -1,45 +1,9 @@
 import os
-
-import discord
 from discord.ext import commands
-from dotenv import load_dotenv
 
-import ids
+from bot.bot import *
 
+os.chdir("./")
 
-class NUCATSBot(commands.Bot):
-    user: discord.ClientUser
-
-    def __init__(self, *, intents: discord.Intents):
-        super().__init__(command_prefix="!", intents=intents)
-
-    print("Loading extensions...")
-    async def setup_hook(self):
-        for extension in (
-            "cogs.auth",
-            "cogs.committee",
-            "cogs.general",
-            "cogs.member_events",
-        ):
-            print(f"Loading extension: {extension}")
-            await self.load_extension(extension)
-            print(f"Loaded extension: {extension}")
-
-    
-        self.tree.copy_global_to(guild=discord.Object(id=ids.server_id))
-        await self.tree.sync(guild=discord.Object(id=ids.server_id))
-
-print("Getting dotenv")
-load_dotenv()
-token = os.getenv("CLIENT_TOKEN")
-print("Starting bot...")
-intents = discord.Intents.all()
-
-client = NUCATSBot(intents=intents)
-
-
-@client.event
-async def on_ready():
-    print(f"Bot is ready. Logged in as {client.user}")
-
-client.run(token)  # type: ignore
+disc_bot = Bot()
+disc_bot.initialize()
